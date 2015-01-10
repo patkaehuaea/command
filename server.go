984d5e77732313352b6326bcfed561b5604d00e1
kaehp001 <pat.kaehuaea@disney.com>
2015-01-10 14:51:58 -0800

    added copyright message

--- server.go
+++ server.go
@@ -1,3 +1,10 @@
+/* Copyright (C) Pat Kaehuaea - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Elmer Fudd <efudd@yoyodyne.com>, September 1943
+ */
+
+
 package main
 
 import (
