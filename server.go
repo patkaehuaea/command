88b6bfe78baefb4f1096235fe1f58e3818097f94
kaehp001 <pat.kaehuaea@gmail.com>
2015-01-11 16:24:48 -0800

    upd version

--- server.go
+++ server.go
@@ -181,7 +181,7 @@ func init() {
 }
 func main() {
 
-	const VERSION_NUMBER = "v1.0.1"
+	const VERSION_NUMBER = "v1.0.2"
 
 	portPtr := flag.String("port", "8080", "Web server binds to this port. Default is 8080.")
 	verbosePtr := flag.Bool("V", false, "Prints version number of program.")
