72fdd5a327ce12449a66f7c20b0bb7ae12aaabae
kaehp001 <pat.kaehuaea@gmail.com>
2015-01-13 17:52:28 -0800

    Cleaned up helper functions and added comments.

--- server.go
+++ server.go
@@ -1,8 +1,13 @@
-/* Copyright (C) Pat Kaehuaea - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- * Written by Pat Kaehuaea, January 2015
- */
+//  Copyright (C) Pat Kaehuaea - All Rights Reserved
+//  Unauthorized copying of this file, via any medium is strictly prohibited
+//  Proprietary and confidential
+//  Written by Pat Kaehuaea, January 2015
+
+// Package contains simple web server that binds to port 8080. Exectuable accepts
+// two parameters, --port to designate listen port, and -V to output the version
+// number of the program. Server responsds to only one request at /time and responds
+// with the current time. All other requests should generate an http 404 status and
+// custom not found page.
 
 package main
 
@@ -18,27 +23,18 @@ import (
 )
 
 //credit: http://stackoverflow.com/questions/17206467/go-how-to-render-multiple-templates-in-golang
-var templates = template.Must(template.ParseGlob(htmlTemplPath()))
+var cwd, _ = os.Getwd()
+var templates = template.Must(template.ParseGlob(filepath.Join(cwd, "templates", "*.html")))
 
-func htmlTemplPath() string {
-	curDir, _ := os.Getwd()
-	templatesPath := filepath.Join(curDir, "templates", "*.html")
-	return templatesPath
-}
-
-func getTime() string {
-	const layout = "3:04:05 PM"
-	t := time.Now().Format(layout)
-	return t
-}
+const timeLayout = "3:04:05 PM"
 
 func timeHandler(w http.ResponseWriter, r *http.Request) {
-	templates.ExecuteTemplate(w, "time", getTime())
+	templates.ExecuteTemplate(w, "time.html", time.Now().Format(timeLayout))
 }
 
 func notFound(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
-	templates.ExecuteTemplate(w, "http404", nil)
+	templates.ExecuteTemplate(w, "http404.html", nil)
 }
 
 func main() {
@@ -56,6 +52,8 @@ func main() {
 	}
 
 	//credit: http://stackoverflow.com/questions/9996767/showing-custom-404-error-page-with-standard-http-package
+	// The gorilla web toolkit (http://www.gorillatoolkit.org/) seems like it provides a cleaner way
+	// to handle and gives some additional functionality as number of paths increases.
 	r := mux.NewRouter()
 	r.HandleFunc("/time", timeHandler)
 	r.NotFoundHandler = http.HandlerFunc(notFound)
