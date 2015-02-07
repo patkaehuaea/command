15e305fd05c76fe8e640a0669ccf5802d69b6a00
kaehp001 <pat.kaehuaea@disney.com>
2015-02-06 22:24:15 -0800

    Added delay() function to time handler and updated comments with timeserver: prefix.

--- timeserver/timeserver.go
+++ timeserver/timeserver.go
@@ -21,6 +21,7 @@ import (
 	"github.com/patkaehuaea/command/timeserver/cookie"
 	"github.com/patkaehuaea/command/timeserver/people"
 	"html/template"
+	"math/rand"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -38,12 +39,22 @@ const (
 )
 
 var (
-	templates *template.Template
+	templates  *template.Template
 	authClient *auth.AuthClient
 )
 
+// Credit: http://goo.gl/MsxPHk
+func delay(average float64, deviation float64) {
+	avgMS := time.Duration(average) * time.Millisecond
+	devMS := time.Duration(deviation) * time.Millisecond
+	random := rand.NormFloat64()
+	load := time.Duration(random)*devMS + avgMS
+	log.Debug("timeserver: Sleeping for " + load.String() + ".")
+	time.Sleep(load)
+}
+
 func getUUIDThenName(r *http.Request) (name string, err error) {
-	log.Info("Called getUUIDThenname function.")
+	log.Info("timeserver: Called getUUIDThenname function.")
 	uuid, uuidErr := cookie.UUID(r)
 	if uuidErr != nil {
 		log.Warn(uuidErr)
@@ -61,26 +72,26 @@ func getUUIDThenName(r *http.Request) (name string, err error) {
 }
 
 func handleDefault(w http.ResponseWriter, r *http.Request) {
-	log.Info("Default handler called.")
+	log.Info("timeserver: Default handler called.")
 	if name, err := getUUIDThenName(r); err != nil {
 		http.Redirect(w, r, "/login", http.StatusFound)
 	} else {
-		log.Debug(name + " viewing site.")
+		log.Debug("timeserver: + " + name + " viewing site.")
 		renderTemplate(w, "greetings", name)
 	}
 }
 
 func handleDisplayLogin(w http.ResponseWriter, r *http.Request) {
-	log.Info("Display login handler called.")
+	log.Info("timeserver: Display login handler called.")
 	renderTemplate(w, "login", "What is your name, Earthling?")
 }
 
 func handleProcessLogin(w http.ResponseWriter, r *http.Request) {
-	log.Info("Process login handler called.")
+	log.Info("timeserver: Process login handler called.")
 	name := r.FormValue("name")
 
 	if valid := people.IsValidName(name); valid {
-		log.Debug("Name matched regex.")
+		log.Debug("timeserver: Name matched regex.")
 		uuid := people.UUID()
 		if err := authClient.Set(uuid, name); err != nil {
 			log.Error(err)
@@ -91,29 +102,33 @@ func handleProcessLogin(w http.ResponseWriter, r *http.Request) {
 		} else {
 			http.SetCookie(w, cookie.NewCookie(uuid, cookie.MAX_AGE))
 			http.Redirect(w, r, "/", http.StatusFound)
-			log.Info(name + " registered on site.")
+			log.Info("timeserver: " + name + " registered on site.")
 		}
 	} else {
-		log.Debug("Invalid username or registration failed.")
+		log.Debug("timeserver: Invalid username or registration failed.")
 		w.WriteHeader(http.StatusBadRequest)
 		renderTemplate(w, "login", "C'mon, I need a name.")
 	}
 }
 
 func handleLogout(w http.ResponseWriter, r *http.Request) {
-	log.Info("Logout handler called.")
+	log.Info("timeserver: Logout handler called.")
 	http.SetCookie(w, cookie.NewCookie("deleted", cookie.DELETE_AGE))
 	renderTemplate(w, "logged-out", nil)
 }
 
 func handleNotFound(w http.ResponseWriter, r *http.Request) {
-	log.Info("Not found handler called.")
+	log.Info("timeserver: Not found handler called.")
 	w.WriteHeader(http.StatusNotFound)
 	renderTemplate(w, "404", nil)
 }
 
 func handleTime(w http.ResponseWriter, r *http.Request) {
-	log.Info("Time handler called.")
+	log.Info("timeserver: Time handler called.")
+
+	// Simulate load with delay function.
+	delay(float64(*config.AvgRespMS), float64(*config.DeviationMS))
+
 	name, _ := getUUIDThenName(r)
 	// If name is blank, template will not render
 	// personalized greeting.
@@ -128,7 +143,7 @@ func handleTime(w http.ResponseWriter, r *http.Request) {
 // credit: http://tinyurl.com/kwc4hls
 func logFileRequest(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Info("File server called.")
+		log.Info("timeserver: File server called.")
 		h.ServeHTTP(w, r)
 	})
 }
@@ -137,7 +152,7 @@ func logFileRequest(h http.Handler) http.Handler {
 func renderTemplate(w http.ResponseWriter, templ string, d interface{}) {
 	err := templates.ExecuteTemplate(w, templ+TEMPL_FILE_EXTENSION, d)
 	if err != nil {
-		log.Error("Error looking for template: " + templ)
+		log.Error("timeserver: Error looking for template: " + templ)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
