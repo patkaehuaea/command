a84f9ce242e39994051f5d0cc348feae52284f29
kaehp001 <pat.kaehuaea@disney.com>
2015-02-06 22:56:23 -0800

    Initial commit of stats package for counting number of concurrent requests.

---
+++ stats/stats.go
@@ -0,0 +1,42 @@
+//  Copyright (C) Pat Kaehuaea - All Rights Reserved
+//  Unauthorized copying of this file, via any medium is strictly prohibited
+//  Proprietary and confidential
+//  Written by Pat Kaehuaea, February 2015
+
+package people
+
+import (
+    "sync"
+)
+
+const )
+    START_VALUE = 0
+    CONCURRENT_REQUESTS = "concurret requests"
+)
+type Stats struct {
+    sync.RWMutex
+    m map[string]*Person
+}
+
+func NewStats() (s *Stats) {
+    s := {m: make(map[string]int)}
+    s.m[CONCURRENT_REQUESTS] = START_VALUE
+}
+
+func (s *Stats) Add() {
+    u.Lock()
+    u.m[CONCURRENT_REQUESTS] = u.m[CONCURRENT_REQUESTS]++
+    u.Unlock()
+}
+
+func (s *Stats) Subtract() {
+    u.Lock()
+    u.m[CONCURRENT_REQUESTS] = u.m[CONCURRENT_REQUESTS]--
+    u.Unlock()
+}
+
+func (s *Stats) Current() (current int){
+    u.Lock()
+    current = u.m[CONCURRENT_REQUESTS]
+    u.Unlock()
+}
