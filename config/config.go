f2509e97b1d8ec9147c966eb0afa6060ae589ebf
kaehp001 <pat.kaehuaea@disney.com>
2015-02-14 15:53:38 -0800

    Updated all timeout parameters to take flag.Duration instead of flag.Int or flag.String.

--- config/config.go
+++ config/config.go
@@ -2,15 +2,16 @@ package config
 
 import (
 	"flag"
+	"time"
 )
 
 const (
 	AUTH_HOST        = "localhost"
 	AUTH_PORT        = ":9080"
-	AUTH_TIMEOUT_MS  = 1000
-	AVG_RESP_MS      = 1000
-	CHECKPOINT_INT   = 0
-	DEV_MS           = 100
+	AUTH_TIMEOUT_MS  = 1000 * time.Millisecond
+	AVG_RESP_MS      = 1000 * time.Millisecond
+	CHECKPOINT_INT   = 60 * time.Second
+	DEV_MS           = 100 * time.Millisecond
 	DUMP_FILE        = ""
 	MAX_IN_FLIGHT    = 0
 	TIME_PORT        = ":8080"
@@ -22,11 +23,11 @@ const (
 var (
 	AuthHost      *string
 	AuthPort      *string
-	AuthTimeoutMS *int
-	AvgRespMS     *int
-	DeviationMS   *int
+	AuthTimeoutMS *time.Duration
+	AvgRespMS     *time.Duration
+	DeviationMS   *time.Duration
 	DumpFile      *string
-	CheckpointInt *int
+	CheckpointInt *time.Duration
 	LogConf       *string
 	MaxInFlight   *int
 	TimePort      *string
@@ -37,9 +38,9 @@ var (
 func init() {
 	// Parameters for timeserver:
 	AuthHost = flag.String("authhost", AUTH_HOST, "Hostname of downstream authentication server.")
-	AuthTimeoutMS = flag.Int("authtimeout-ms", AUTH_TIMEOUT_MS, "Milliseconds to wait before terminating downstream auth request.")
-	AvgRespMS = flag.Int("avg-response-ms", AVG_RESP_MS, "Average time to delay response to upstream time request.")
-	DeviationMS = flag.Int("deviation-ms", DEV_MS, "Average standard deviation in response delay to upstream time request.")
+	AuthTimeoutMS = flag.Duration("authtimeout-ms", AUTH_TIMEOUT_MS, "Milliseconds to wait before terminating downstream auth request.")
+	AvgRespMS = flag.Duration("avg-response-ms", AVG_RESP_MS, "Average time to delay response to upstream time request.")
+	DeviationMS = flag.Duration("deviation-ms", DEV_MS, "Average standard deviation in response delay to upstream time request.")
 	LogConf = flag.String("log", SEELOG_CONF_FILE, "Name of log configuration file in etc directory relative to executable.")
 	MaxInFlight = flag.Int("max-inflight", MAX_IN_FLIGHT, "Maximum number of in-flight time requests the timeserver can handle.")
 	TimePort = flag.String("port", TIME_PORT, "Time server binds to this port.")
@@ -48,7 +49,7 @@ func init() {
 
 	// Parameters for authserver:
 	DumpFile = flag.String("dumpfile", DUMP_FILE, "Name of file storing state as JSON document.")
-	CheckpointInt = flag.Int("checkpoint-interval", CHECKPOINT_INT, "Dump state to file every checkpoint-interval seconds.")
+	CheckpointInt = flag.Duration("checkpoint-interval", CHECKPOINT_INT, "Dump state to file every checkpoint-interval seconds.")
 
 	// Shared parameters:
 	AuthPort = flag.String("authport", AUTH_PORT, "Auth server binds to this port.")
