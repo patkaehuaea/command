a7313beec9a4b249e0503dd462c0b179e4d6bbed
kaehp001 <pat.kaehuaea@disney.com>
2015-02-15 14:51:10 -0800

    Updated start to tail two logs without failing.

--- start.sh
+++ start.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-
-AUTHSERVER_LOG="$GOPATH/src/github.com/patkaehuaea/command/authserver/out/authserver.log"
-TIMSERVER_LOG="$GOPATH/src/github.com/patkaehuaea/command/timeserver/out/timeserver.log"
-
 echo "Starting authserver as background process..."
 cd $GOPATH/src/github.com/patkaehuaea/command/authserver
 $GOPATH/bin/authserver --dumpfile ~/users.json > /dev/null 2>&1 &
@@ -11,7 +7,7 @@ if [ "$?" -eq 0 ] ; then
     echo "Starting timeserver as background process.."
     cd $GOPATH/src/github.com/patkaehuaea/command/timeserver
     $GOPATH/bin/timeserver > /dev/null 2>&1 &
-    tail -f $AUTHSERVER_LOG -f $TIMESERVER_LOG
+    tail -f $GOPATH/src/github.com/patkaehuaea/command/*/out/*.log
 else
     echo "Failed to start authserver. Will not start timeserver."
 fi
