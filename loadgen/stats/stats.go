17da1636de7dbb95834da53cab3f91684e40b907
kaehp001 <pat.kaehuaea@disney.com>
2015-02-27 21:32:54 -0800

    Changed String() method to Print().

--- loadgen/stats/stats.go
+++ loadgen/stats/stats.go
@@ -6,7 +6,7 @@
 package stats
 
 import (
-	"strconv"
+	"fmt"
 	"sync"
 )
 
@@ -21,55 +21,61 @@ const (
 	START_VALUE = 0
 )
 
-type Statistics struct {
+type Counter struct {
 	sync.RWMutex
 	counters map[string]int
 }
 
-func New() (s *Statistics) {
-	s = &Statistics{counters: make(map[string]int)}
-	s.counters[TOTAL_KEY] = START_VALUE
-	s.counters[KEY_100] = START_VALUE
-	s.counters[KEY_200] = START_VALUE
-	s.counters[KEY_300] = START_VALUE
-	s.counters[KEY_400] = START_VALUE
-	s.counters[KEY_500] = START_VALUE
-	s.counters[ERROR_KEY] = START_VALUE
+func New() (c *Counter) {
+	c = &Counter{counters: make(map[string]int)}
+	c.counters[TOTAL_KEY] = START_VALUE
+	c.counters[KEY_100] = START_VALUE
+	c.counters[KEY_200] = START_VALUE
+	c.counters[KEY_300] = START_VALUE
+	c.counters[KEY_400] = START_VALUE
+	c.counters[KEY_500] = START_VALUE
+	c.counters[ERROR_KEY] = START_VALUE
 	return
 }
 
-func (s *Statistics) Copy() (copy map[string]int) {
+func (c *Counter) Copy() (copy map[string]int) {
 	copy = make(map[string]int)
-	s.RLock()
-	for statusCode, count := range s.counters {
+	c.RLock()
+	for statusCode, count := range c.counters {
 		copy[statusCode] = count
 	}
-	s.RUnlock()
+	c.RUnlock()
 	return
 }
 
-func (s *Statistics) Increment(statistic string, delta int) (err error) {
-	s.Lock()
-	s.counters[TOTAL_KEY] = s.counters[TOTAL_KEY] + delta
-	s.counters[statistic] = s.counters[statistic] + delta
-	s.Unlock()
+func (c *Counter) Get(statistic string) (count int) {
+	c.RLock()
+	count = c.counters[statistic]
+	c.RUnlock()
 	return
 }
 
-func (s *Statistics) Reset(statistic string) {
-	s.Lock()
-	s.counters[statistic] = START_VALUE
-	s.Unlock()
+func (c *Counter) Increment(statistic string, delta int) {
+	c.Lock()
+	c.counters[TOTAL_KEY] = c.counters[TOTAL_KEY] + delta
+	c.counters[statistic] = c.counters[statistic] + delta
+	c.Unlock()
 }
 
-func (s *Statistics) String() (output string) {
-	copy := s.Copy()
-	output = TOTAL_KEY + ":\t" + strconv.Itoa(copy[TOTAL_KEY]) + "\n"
-	output = output + KEY_100 + ":\t" + strconv.Itoa(copy[KEY_100]) + "\n"
-	output = output + KEY_200 + ":\t" + strconv.Itoa(copy[KEY_200]) + "\n"
-	output = output + KEY_300 + ":\t" + strconv.Itoa(copy[KEY_300]) + "\n"
-	output = output + KEY_400 + ":\t" + strconv.Itoa(copy[KEY_400]) + "\n"
-	output = output + KEY_500 + ":\t" + strconv.Itoa(copy[KEY_500]) + "\n"
-	output = output + ERROR_KEY + ":\t" + strconv.Itoa(copy[ERROR_KEY]) + "\n"
+func (c *Counter) Reset(statistic string) {
+	c.Lock()
+	c.counters[statistic] = START_VALUE
+	c.Unlock()
+}
+
+func (c *Counter) Print() (output string) {
+	copy := c.Copy()
+	fmt.Printf("%s:\t %d\n", TOTAL_KEY, copy[TOTAL_KEY])
+	fmt.Printf("%s:\t %d\n", KEY_100, copy[KEY_100])
+	fmt.Printf("%s:\t %d\n", KEY_200, copy[KEY_200])
+	fmt.Printf("%s:\t %d\n", KEY_300, copy[KEY_300])
+	fmt.Printf("%s:\t %d\n", KEY_400, copy[KEY_400])
+	fmt.Printf("%s:\t %d\n", KEY_500, copy[KEY_500])
+	fmt.Printf("%s:\t %d\n", ERROR_KEY, copy[ERROR_KEY])
 	return
 }
